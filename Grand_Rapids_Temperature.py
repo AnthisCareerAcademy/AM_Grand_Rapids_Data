b1b4420dae1c48879812a149bd968c8790be33c7
DominicD2027 <dominic.dambrosio@fwcsstudents.org>
2026-04-27 09:41:18 -0400

    Add files via upload
    The full file for the Grand Rapids, MI average temperature linegraph.

---
+++ Grand_Rapids_Temperature.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# Reading from the Grand Rapids file.
+path = Path('eq_data/a - Copy (2).csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+dates, temps = [], []
+
+for row in reader:
+    try:
+        current_date = datetime.strptime(row[1], '%Y-%m-%d')
+        high = float(row[2])
+        low = float(row[3])
+        current_temp = (high + low) / 2
+
+    except (ValueError, IndexError):
+        continue
+
+    else:
+        dates.append(current_date)
+        temps.append(current_temp)
+
+plt.style.use('seaborn-v0_8')
+
+fig, ax = plt.subplots(figsize=(12, 8))
+
+ax.plot(dates, temps, color='orange', linewidth=2)
+
+ax.set_title('Daily Average Temperature (Aug 13–18, 10 Years)', fontsize=24)
+ax.set_xlabel('Date', fontsize=16)
+ax.set_ylabel('Avg Temperature (F)', fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
